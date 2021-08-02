2da2f8269215cd56ac82ed84d9b2e5a08db54a71
andreymashko7 <63639567+andreymashko7@users.noreply.github.com>
2021-08-02 18:34:59 +0300

    simple todo-app, done

---
+++ src/Loader.js
@@ -0,0 +1,7 @@
+export default function Loader(params) {
+  return (
+    <div style={{ display: "flex", justifyContent: "center", margin: ".5rem" }}>
+      <div class="lds-dual-ring"></div>
+    </div>
+  );
+}
