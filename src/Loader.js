3a40dc8a12e8856a3243fe04c2001cc068b3728b
andreymashko7 <63639567+andreymashko7@users.noreply.github.com>
2021-09-05 22:07:45 +0300

    fixed error log

--- src/Loader.js
+++ src/Loader.js
@@ -1,7 +1,7 @@
 export default function Loader(params) {
   return (
     <div style={{ display: "flex", justifyContent: "center", margin: ".5rem" }}>
-      <div class="lds-dual-ring"></div>
+      <div className="lds-dual-ring"></div>
     </div>
   );
 }
